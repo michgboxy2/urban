b314a2cad0dc80a795bf25879ef524a7b8f672d6
michgboxy <michgboxy@gmail.com>
2020-10-18 17:36:37 +0100

    redis setup

---
+++ src/urbanDistrict/cache.ts
@@ -0,0 +1,18 @@
+import redis from "redis";
+
+const RedisHost = process.env.RedisHost || "127.0.0.1";
+const RedisPort = Number(process.env.RedisPort) || 6379;
+
+export const client = redis.createClient(RedisPort, RedisHost);
+
+client.on("connect", function (error) {
+  console.log("redis connected");
+});
+
+client.on("error", function (error) {
+  console.log(error);
+});
+
+client.on("end", function (error) {
+  console.log("redis disconnected");
+});
