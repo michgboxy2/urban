8e72c09a29addb153d958302b9cc72b6013699f2
michgboxy <michgboxy@gmail.com>
2020-10-18 17:37:33 +0100

    cache test

--- src/urbanDistrict/__test__/district/district.test.ts
+++ src/urbanDistrict/__test__/district/district.test.ts
@@ -1,5 +1,7 @@
 import request from "supertest";
 import { app } from "../../../app";
+import { client } from "../../cache";
+import { checkCache } from "../../districtController";
 
 it("returns error 400 if address is not provided", async () => {
   await request(app).post("/api/district").send({}).expect(400);
@@ -24,4 +26,17 @@ it("returns a not found result if address passed is not within the districts", a
   expect(response.status).toEqual(200);
   expect(response.body).toHaveProperty("status");
   expect(response.body.status).toEqual("NOT_FOUND");
+});
+
+it("fetches response from cache if address exists in cache", async () => {
+  const address = "A420, Bristol BS2, UK";
+  let response = await request(app).post("/api/district").send({
+    address,
+  });
+
+  client.get(address, (err, data) => {
+    if (data != null) {
+      expect(response.body).toEqual(JSON.parse(data));
+    }
+  });
 });
